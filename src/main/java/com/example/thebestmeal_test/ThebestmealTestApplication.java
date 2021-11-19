d3f6a1dc3785ce3094506be49ea2b9ed9b45deec
Lukaid <crrescent3859@gmail.com>
2021-11-20 02:05:09 +0900

    start project

---
+++ src/main/java/com/example/thebestmeal_test/ThebestmealTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.thebestmeal_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThebestmealTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThebestmealTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/thebestmeal_test/ThebestmealTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thebestmeal_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThebestmealTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
