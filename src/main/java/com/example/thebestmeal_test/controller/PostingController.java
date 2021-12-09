5fb210d8fba84a32c53bb12378e5b7bc54e9a9c9
Lukaid <crrescent3859@gmail.com>
2021-12-10 01:17:47 +0900

    posting refactoring

--- src/main/java/com/example/thebestmeal_test/controller/PostingController.java
+++ src/main/java/com/example/thebestmeal_test/controller/PostingController.java
@@ -1,9 +1,5 @@
 package com.example.thebestmeal_test.controller;
 
-import com.example.thebestmeal_test.domain.Food;
-import com.example.thebestmeal_test.domain.Posting;
-import com.example.thebestmeal_test.domain.Tag;
-import com.example.thebestmeal_test.domain.User;
 import com.example.thebestmeal_test.dto.FoodCheckDto;
 import com.example.thebestmeal_test.dto.PostDto;
 import com.example.thebestmeal_test.repository.FoodRepository;
@@ -22,9 +18,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.transaction.Transactional;
 import java.io.IOException;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @RequestMapping({"/api"})
@@ -40,46 +33,13 @@ public class PostingController {
 
     @Transactional
     @PostMapping({"/post"})
-
     public String postFood(PostDto postDto, @AuthenticationPrincipal UserDetailsImpl userDetails) throws IOException {
-
-        User user = (User) this.userRepository.findByUsername(userDetails.getUsername()).orElseThrow(() -> {
-            return new NullPointerException("그런 사람 없는데요?");
-        });
-//       food 저장
-        String foodImgUrl = this.awsService.uploadFoodImg(postDto.getFoodImgUrl());
-        Food food = new Food(postDto, foodImgUrl);
-        this.foodRepository.save(food);
-        //category 저장
-//        List<String> items1 = Arrays.asList(postDto.getPostingTag());
-        List<Tag> tags1 = (List) postDto.getPostingEmo().stream().map((tag) -> {
-            return new Tag(food, tag, "emotion");
-        }).collect(Collectors.toList());
-        this.tagRepository.saveAll(tags1);
-        //emotion 저장
-        List<Tag> tags2 = (List) postDto.getPostingCat().stream().map((tag) -> {
-            return new Tag(food, tag, "category");
-        }).collect(Collectors.toList());
-        this.tagRepository.saveAll(tags2);
-        Food food2 = (Food) this.foodRepository.findByName(postDto.getPostingFoodName()).get();
-        Posting posting = new Posting(postDto, user, food2);
-        this.postingRepository.save(posting);
-
-        return foodImgUrl;
-
+        return postingService.toPostFoodService(postDto, userDetails);
     }
-
-    //controller 분리 시도 중
-//    public String postFood(PostDto postDto, @AuthenticationPrincipal UserDetailsImpl userDetails) throws IOException {
-//        return postingService.postFoodService(postDto, userDetails);}
 
     @PostMapping({"/foodcheck"})
     public Boolean foodCheck(@RequestBody FoodCheckDto foodCheckDto) {
-        String postingFoodName = foodCheckDto.getPostingFoodName();
-        Optional<Food> found = this.foodRepository.findByName(postingFoodName);
-        Boolean response = found.isPresent();
-        System.out.println(response);
-        return response;
+        return postingService.foodDupCheck(foodCheckDto);
     }
 }
 
