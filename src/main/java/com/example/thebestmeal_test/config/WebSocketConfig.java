0def8aceb11a7df7cd5818e85873f6c7ebdd0b2c
hankug <hankug1@naver.com>
2021-12-21 15:27:10 +0900

    edit

---
+++ src/main/java/com/example/thebestmeal_test/config/WebSocketConfig.java
@@ -0,0 +1,10 @@
+//package com.example.thebestmeal_test.config;
+//
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Configuration;
+//
+//@RequiredArgsConstructor
+//@Configuration
+//public class WebSocketConfig {
+//}
