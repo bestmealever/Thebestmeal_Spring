58e465dd69790e753a49b780840156122a008802
장민 <90808711+projectjjj@users.noreply.github.com>
2021-12-27 16:44:56 +0900

    커뮤니티 api - service, controller

--- src/main/java/com/example/thebestmeal_test/community/ArticleController.java
+++ src/main/java/com/example/thebestmeal_test/community/ArticleController.java
@@ -1,19 +1,99 @@
 package com.example.thebestmeal_test.community;
 
+import com.example.thebestmeal_test.domain.User;
+import com.example.thebestmeal_test.repository.TagRepository;
+import com.example.thebestmeal_test.responseEntity.BasicResponse;
+import com.example.thebestmeal_test.responseEntity.CommonResponse;
+import com.example.thebestmeal_test.responseEntity.ErrorResponse;
 import com.example.thebestmeal_test.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.parameters.P;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Objects;
 
 @RequiredArgsConstructor
 @RestController
 public class ArticleController {
     private final ArticleService articleService;
+    private final ArticleRepository articleRepository;
+    private final CommentRepository commentRepository;
+    private final TagRepository tagRepository;
+
     @PostMapping("/article")
-    public String postArticle(@RequestBody ArticleDto articleDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        return articleService.postArticle(articleDto,userDetails);
+    public ResponseEntity<Message> postArticle(ArticleDto articleDto, @AuthenticationPrincipal UserDetailsImpl userDetails) throws IOException {
+        Article article = articleService.postArticle(articleDto,userDetails);
+        Message message = new Message();
+        HttpHeaders headers= new HttpHeaders();
+        headers.setContentType(new MediaType("application", "json", StandardCharsets.UTF_8));
+        ResponseEntity<Message> response = new ResponseEntity<>(message, headers, HttpStatus.OK);
+        if (userDetails == null) {
+            message.setStatus(StatusEnum.BAD_REQUEST);
+            message.setMessage("로그인 후 이용해주세요");
+        }
+        if(article.getContent() == null) {
+            message.setStatus(StatusEnum.BAD_REQUEST);
+            message.setMessage("글 내용을 작성해주세요.");
+        } else {
+            message.setStatus(StatusEnum.OK);
+            message.setMessage("글 작성 성공");
+            message.setData(article);
+        }
+        return response;
+    }
+
+//    @GetMapping("/articles")
+//    private List<Article> getArticle() {
+//        return articleRepository.findAll();
+//    }
+
+    @PostMapping("/article/comment")
+    public String postComment(@RequestBody CommentDto commentDto, @AuthenticationPrincipal UserDetailsImpl userDetails) throws Exception {
+        if (userDetails == null) {
+            return articleService.postComment(commentDto);
+        }
+        return articleService.postComment(commentDto,userDetails);
+    }
+
+    @DeleteMapping("/article/{id}")
+    public String deleteArticle(@PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails) throws Exception {
+        Long userId = userDetails.getUser().getId();
+        Article article = articleRepository.findById(id).orElseThrow(() -> new NullPointerException("없음"));
+        Long articleId = article.getUser().getId();
+        if (Objects.equals(articleId, userId)) {
+            articleRepository.delete(article);
+            return "삭제 완료!";
+        } else if (!Objects.equals(articleId, userId)) {
+            return "자신이 작성한 글만 삭제 가능합니다.";
+        }
+        return "게시글을 삭제할 수 없습니다.";
+    }
+
+    @DeleteMapping("/article/comment/{id}")
+    public String deleteComment(@PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails) throws Exception {
+        Long userId = userDetails.getUser().getId();
+        Comment comment = commentRepository.findById(id).orElseThrow(() -> new NullPointerException("없음"));
+        Long commentUserId = comment.getUser().getId();
+        if (Objects.equals(commentUserId, userId)) {
+            commentRepository.delete(comment);
+            return "삭제 완료!";
+        }
+        return "자신이 작성한 댓글만 삭제 가능합니다.";
+    }
+
+    @GetMapping("/articles/{id}")
+    public Article getArticles(@PathVariable Long id) {
+        return articleRepository.findById(id).orElseThrow(() -> new NullPointerException("없슈"));
     }
 }
--- src/main/java/com/example/thebestmeal_test/community/ArticleService.java
+++ src/main/java/com/example/thebestmeal_test/community/ArticleService.java
@@ -1,5 +1,6 @@
 package com.example.thebestmeal_test.community;
 
+import com.example.thebestmeal_test.Error.CustomException;
 import com.example.thebestmeal_test.domain.Food;
 import com.example.thebestmeal_test.domain.Tag;
 import com.example.thebestmeal_test.domain.User;
@@ -7,35 +8,126 @@ import com.example.thebestmeal_test.repository.FoodRepository;
 import com.example.thebestmeal_test.repository.TagRepository;
 import com.example.thebestmeal_test.repository.UserRepository;
 import com.example.thebestmeal_test.security.UserDetailsImpl;
+import com.example.thebestmeal_test.service.AwsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.nio.charset.Charset;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static com.example.thebestmeal_test.Error.ErrorCode.FOOD_NOT_FOUND;
+import static com.example.thebestmeal_test.Error.ErrorCode.MEMBER_NOT_FOUND;
+import static com.example.thebestmeal_test.community.ArticleType.*;
 
 @RequiredArgsConstructor
 @Service
 public class ArticleService {
     private final FoodRepository foodRepository;
-    private final TagRepository tagRepository;
+    private final ArticleTagRepository articleTagRepository;
     private final UserRepository userRepository;
+    private final ArticleRepository articleRepository;
+    private final VoteRepository voteRepository;
+    private final CommentRepository commentRepository;
+    private final AwsService awsService;
 
-    public String postArticle(ArticleDto articleDto, UserDetailsImpl userDetails) {
+    @Transactional
+    public Article postArticle(ArticleDto articleDto, UserDetailsImpl userDetails) throws IOException {
+        String url = null;
+        if(articleDto.getImage() != null) url = awsService.uploadCommunityImg(articleDto.getImage());
+        ArticleType articleType = articleDto.getArticleType();
+        Article savedArticle = new Article();
         String title = articleDto.getTitle();
         String content = articleDto.getContent();
-        ArticleType articleType = articleDto.getArticleType();
-        String tagText= articleDto.getTags();
-        User user = userRepository.findById(userDetails.getUser().getId()).orElseThrow(()->new NullPointerException("그런 사람 없음"));
-        Food food = foodRepository.findById(articleDto.getFoodidx()).orElseThrow(() -> new NullPointerException("음식 찾기 못함"));
-        createTag(tagText,food);
-        new Article(title, content, articleType, food, user);
-        return "등록 완료여";
+        String x = articleDto.getX();
+        String y = articleDto.getY();
+        String placeName = articleDto.getPlaceName();
+        String placeAddress = articleDto.getPlaceAddress();
+        String placeUrl = articleDto.getPlaceUrl();
+        User user = userRepository.findById(userDetails.getUser().getId()).orElseThrow(() -> new CustomException(MEMBER_NOT_FOUND));
+        switch(articleType)
+        {
+            case recipe:
+                String tagText= articleDto.getTags();
+                Food food = foodRepository.findById(articleDto.getFoodidx()).orElseThrow(() -> new CustomException(FOOD_NOT_FOUND));
+                Article article1 = new Article(title, content, articleType, food, user, url);
+                savedArticle = articleRepository.save(article1);
+                createTag(tagText,article1);
+                break;
+            case vote:
+                String option1Name = articleDto.getOption1Name();
+                String option2Name = articleDto.getOption2Name();
+                Vote vote = new Vote(option1Name, option2Name);
+                voteRepository.save(vote);
+                Article article2 = new Article(title, content, articleType, user, vote, url);
+                savedArticle = articleRepository.save(article2);
+                break;
+            case chat:
+                Article article3 = new Article(title, content, articleType, user, url);
+                savedArticle = articleRepository.save(article3);
+                break;
+            case place:
+                Article article4 = new Article(title, content, articleType, user, x, y, placeName, placeAddress, placeUrl, url);
+                savedArticle = articleRepository.save(article4);
+                break;
+        }
+        return savedArticle;
     }
 
-    public void createTag(String tagText, Food food) {
+    public void createTag(String tagText, Article article) {
         List<String> tagList = Arrays.asList(tagText.split("\\s*,\\s*"));
-        List<Tag> tags= tagList.stream().map(tag -> new Tag(food, tag,"article")).collect(Collectors.toList());
-        tagRepository.saveAll(tags);
+        List<ArticleTag> tags= tagList.stream().map(tag -> new ArticleTag(article, tag)).collect(Collectors.toList());
+        articleTagRepository.saveAll(tags);
+    }
+
+//    public ResponseEntity<?> relatedFood(Long foodid) {
+//        Food food = foodRepository.findById(foodid).orElseThrow(()-> new NullPointerException("음식 없어요"));
+//        if (food == null) {
+//            //그냥 문자로 등록
+//        } {
+//            //food 정보 리턴 -_-
+//        }
+//    }
+
+    public Page<Article> getArticles(int page, int size, String sortBy, boolean isAsc) {
+        Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
+        Sort sort = Sort.by(direction, sortBy);
+        Pageable pageable = PageRequest.of(page, size, sort);
+
+        return articleRepository.findAll(pageable);
+    }
+
+    public Page<Article> getAllArticles(int page, int size, String sortBy, boolean isAsc) {
+        Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
+        Sort sort = Sort.by(direction, sortBy);
+        Pageable pageable = PageRequest.of(page, size, sort);
+
+        return articleRepository.findAll(pageable);
+    }
+
+    public String postComment(CommentDto commentDto) {
+        Article article = articleRepository.findById(commentDto.getArticleIdx()).orElseThrow(()-> new NullPointerException("해당 게시글이 지워졌습니다."));
+        commentRepository.save(new Comment(article, commentDto.getComment()));
+        return "비회원님이 댓글을 남기셨군요";
+    }
+
+    public String postComment(CommentDto commentDto, UserDetails userDetails) throws Exception {
+        User user = userRepository.findByUsername(userDetails.getUsername()).orElseThrow(() -> new NullPointerException("없음"));
+        Article article = articleRepository.findById(commentDto.getArticleIdx()).orElseThrow(()-> new NullPointerException("해당 게시글이 지워졌습니다."));
+        commentRepository.save(new Comment(article, commentDto.getComment(),user));
+        return "회원님이 댓글을 남기셨군요";
     }
 }
