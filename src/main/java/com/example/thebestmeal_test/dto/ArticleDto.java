102371b35d5c6e22283b1bcfdf196f125dacb7c0
장민 <90808711+projectjjj@users.noreply.github.com>
2021-12-30 09:32:50 +0900

    테스트코드 ㅠㅠ...

--- src/main/java/com/example/thebestmeal_test/dto/ArticleDto.java
+++ src/main/java/com/example/thebestmeal_test/dto/ArticleDto.java
@@ -1,10 +1,14 @@
 package com.example.thebestmeal_test.dto;
 
 import com.example.thebestmeal_test.domain.ArticleType;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Setter
 @Getter
 public class ArticleDto {
@@ -22,4 +26,12 @@ public class ArticleDto {
     private String placeName;
     private String placeAddress;
     private String placeUrl;
+
+    public ArticleDto(String title, String content, String option1Name, String option2Name, ArticleType articleType) {
+        this.title = title;
+        this.content = content;
+        this.option1Name = option1Name;
+        this.option2Name = option2Name;
+        this.articleType = articleType;
+    }
 }
---
+++ src/test/java/com/example/thebestmeal_test/service/ArticleServiceTest.java
@@ -0,0 +1,69 @@
+package com.example.thebestmeal_test.service;
+
+import com.example.thebestmeal_test.domain.*;
+import com.example.thebestmeal_test.dto.ArticleDto;
+import com.example.thebestmeal_test.dto.UserDto;
+import com.example.thebestmeal_test.repository.ArticleRepository;
+import com.example.thebestmeal_test.repository.UserRepository;
+import com.example.thebestmeal_test.security.UserDetailsImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.springframework.test.util.AssertionErrors.assertEquals;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+class ArticleServiceTest {
+    @Mock
+    ArticleRepository articleRepository;
+
+    @Mock
+    UserRepository userRepository;
+
+    User nowUser;
+
+    @BeforeEach
+    void beforeEach() throws IOException {
+        User user = new User(
+                "testmokito", "testmokito1", "test@test.com", UserRole.USER
+        );
+        this.nowUser = user;
+        User user1 = userRepository.findByUsername("testmokito").orElseThrow(()->new NullPointerException("아이디 없네요"));
+        assertEquals("유저 생성 확인.", user.getId(), user1.getId());
+    }
+
+    @Test
+    @DisplayName("articleType vote 게시글이 잘 생성되는지 확인")
+    void createArticleVote() throws IOException {
+
+        //given
+        ArticleDto articleDto = new ArticleDto(
+                "이거 투표해주세요", "뭐먹을까요? 정말 고민됩니다", "닭도리탕","김치", ArticleType.vote
+        );
+
+        String url = "";
+        Vote vote = new Vote(articleDto.getOption1Name(), articleDto.getOption2Name());
+        Article article = new Article(articleDto.getTitle(), articleDto.getContent(), articleDto.getArticleType(),nowUser,vote,url);
+
+       //when
+        Article articleTest = articleRepository.findById(article.getId()).orElseThrow(
+                ()->new NullPointerException("글 생성 안됨"));
+
+        // then
+        assertEquals("글 생성 확인.", article.getId(), articleTest.getId());
+    }
+
+
+}
