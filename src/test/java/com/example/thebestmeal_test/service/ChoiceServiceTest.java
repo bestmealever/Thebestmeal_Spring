5d6bcd8a33fb16af81261003dae389ff1183ab2f
hankug <hankug1@naver.com>
2021-12-30 15:50:48 +0900

    test

---
+++ src/test/java/com/example/thebestmeal_test/service/ChoiceServiceTest.java
@@ -0,0 +1,24 @@
+package com.example.thebestmeal_test.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChoiceServiceTest {
+
+    @Test
+    @DisplayName("태그 선택지 저장하기")
+    void toChoiceService() {
+        //given
+        //when
+        //then
+    }
+
+    @Test
+    void noDup() {
+        //given
+        //when
+        //then
+    }
+}
