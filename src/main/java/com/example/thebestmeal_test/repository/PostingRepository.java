9fb4cc4516d19669ba44d247ec9013ec26854589
jieun-dev1 <jkdev1324@gmail.com>
2022-01-03 22:55:26 +0900

    PostingService 추천하기 중복 체크 예외 처리

--- src/main/java/com/example/thebestmeal_test/repository/PostingRepository.java
+++ src/main/java/com/example/thebestmeal_test/repository/PostingRepository.java
@@ -9,13 +9,13 @@ import java.util.List;
 
 public interface PostingRepository extends JpaRepository<Posting,Long> {
     //EntityGraph를 아래와 같이 넣으면 "likedFood"를 못찾아옴 -> mypage가 제대로 뜨지 않는다.
-    @EntityGraph(attributePaths = {"tags","recommendeds","posting","food","user"})
+    @EntityGraph(attributePaths = {"tags","recommendeds","food","user","likedFood"})
     List<Posting> findAllByUserId(Long id);
     //삭제
 //    List<Posting> findAllByStatusIsNull();
 //    List<Posting> findAllByStatus(Status status);
 
-
+    @EntityGraph(attributePaths = {"food","user"})
     List<Posting> findAllByStatus(PostingStatus postingstatus);
 
 //    List<Posting> findAllByPostingStatus(PostingStatus postingstatus);
